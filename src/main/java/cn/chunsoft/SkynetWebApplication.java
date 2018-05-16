28dbae5f38c7e596f2690a56e416ca1dca6f5a54
chunonesoft <chun_soft@qq.com>
2018-05-16 15:06:25 +0800

    第一次提交

---
+++ src/main/java/cn/chunsoft/SkynetWebApplication.java
@@ -0,0 +1,12 @@
+package cn.chunsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkynetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkynetWebApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/chunsoft/SkynetWebApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.chunsoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkynetWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
